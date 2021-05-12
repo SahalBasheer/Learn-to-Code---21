257309d8c7f833f8df7283af6f5e3e84e3890c9f
SahalBasheer <76899304+SahalBasheer@users.noreply.github.com>
2021-05-12 15:16:59 +0530

    Create DAY02_2.C

---
+++ DAY02_2.C
@@ -0,0 +1,16 @@
+//Write a C Program to read an integer and find it's square root.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+int a;
+    float b;
+    scanf("%d", &a);
+    b=sqrt(a);
+    printf("%.1f", b);
+    return 0;
+}
